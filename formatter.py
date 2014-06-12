272ce2e7b73ff1271e70f8c8fea50958dc8a2e5f
jeffposnick <jeffy34@gmail.com>
2014-06-12 00:57:45 -0400

    Google+ photo support
    And a bunch of other stuff.

--- formatter.py
+++ formatter.py
@@ -1,87 +1,167 @@
 #!/usr/bin/python
 
-import argparse
 import datetime
 import httplib2
 import json
 import math
 import os
 import re
+import sys
 
-from wand import exceptions
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import argparser, run_flow
 from wand.color import Color
 from wand.drawing import Drawing
 from wand.image import Image
 
+class GooglePlusImage:
+	DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 
-class PhotoFrameFormatter:
-	FONT = "RobotoCondensed-Regular.ttf"
-	FILL_COLOR = Color("#fff")
-	STROKE_COLOR = Color("#000")
-	FONT_SIZE = 24
-	ORIENTATION_TO_ROTATION = {
-		"8": 270,
-		"3": 180,
-		"6": 90
-	}
+	def __init__(self, http, entry):
+		if "gphoto$originalvideo" in entry:
+			self.image = None
+			return
+
+		headers, content = http.request(entry["media$group"]["media$content"][0]["url"], "GET")
+		self.image = Image(blob=content)
+
+		self.filename = entry["title"]["$t"]
+
+		self.datetime = datetime.datetime.strptime(entry["published"]["$t"], self.DATETIME_FORMAT)
+
+		try:
+			lat_long = entry["georss$where"]["gml$Point"]["gml$pos"]["$t"]
+		except KeyError:
+			self.latitude = self.longitude= None
+		else:
+			self.latitude, self.longitude = lat_long.split(" ")
+
+		self.rotation_degrees = 0
+
+
+class FileImage:
 	DIRECTIONS_TO_SIGNS = {
 		"n": 1,
 		"e": 1,
 		"s": -1,
 		"w": -1
 	}
-	SPLIT_REGEX = re.compile(r",\s*")
 	TAKEN_AT_DATE_FORMAT = "%Y:%m:%d %H:%M:%S"
+
+	def __init__(self, file_path):
+		try:
+			self.image = Image(filename=self.file_path)
+		except Exception:
+			self.image = None
+			return
+
+		self.filename = file_path.replace(os.sep, "").replace(".", "").replace(" ", "") + ".jpg"
+
+		latitude = longitude = latitude_sign = longitude_sign = None
+		for key, value in self.image.metadata.items():
+			lower_key = key.lower()
+			if lower_key == "exif:orientation":
+				if value == "8":
+					self.rotation_degrees = 270
+				elif value == "3":
+					self.rotation_degrees = 180
+				elif value == "6":
+					self.rotation_degrees = 90
+				else:
+					self.rotation_degrees = 0
+			elif lower_key == "exif:gpslatitude":
+				latitude = value
+			elif lower_key == "exif:gpslongitude":
+				longitude = value
+			elif lower_key == "exif:gpslatituderef":
+				latitude_sign = self.DIRECTIONS_TO_SIGNS[value.lower()]
+			elif lower_key == "exif:gpslongituderef":
+				longitude_sign = self.DIRECTIONS_TO_SIGNS[value.lower()]
+			elif lower_key == "exif:datetimeoriginal":
+				self.datetime = datetime.datetime.strptime(value, self.TAKEN_AT_DATE_FORMAT)
+
+		if latitude and longitude and latitude_sign and longitude_sign:
+			self.latitude = self.dms_to_decimal(dms=latitude, sign=latitude_sign)
+			self.longitude = self.dms_to_decimal(dms=longitude, sign=longitude_sign)
+		else:
+			self.latitude = self.longitude= None
+
+	@staticmethod
+	def dms_to_decimal(dms, sign):
+		factor = 1
+		total = 0
+
+		for fraction in re.split(re.compile(r",\s*"), dms):
+			numerator, denominator = fraction.split("/")
+			quotient = float(numerator) / float(denominator) / factor
+			total += quotient
+			factor *= 60
+
+		return sign * total
+
+
+class PhotoFrameFormatter:
+	GOOGLE_PLUS = "google+"
+	CLIENT_SECRETS_FILE = "client_secrets.json"
+	PICASA_WEB_ALBUMS_SCOPE = "https://picasaweb.google.com/data/"
+	PICASA_WEB_ALUMBS_RECENT_PHOTOS_URL = "https://picasaweb.google.com/data/feed/api/user/default?kind=photo&alt=json&imgmax=d&max-results=100"
+	FILL_COLOR = Color("#fff")
+	STROKE_COLOR = Color("#000")
+	STROKE_WIDTH = 0.5
+	FONT_SIZE = 24
+	FONT = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+		"RobotoCondensed-Regular.ttf")
 	DISPLAY_DATE_FORMAT = "%b %d '%y, %H:%M"
 	REVERSE_GEOCDOING_URL_FORMAT = "https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key={key}&result_type=neighborhood"
 
-	def __init__(self, source_dir, output_dir, max_width, max_height, api_key):
-		self.source_dir = source_dir
+	def __init__(self, args, source, output_dir, max_width, max_height, api_key, no_text):
+		self.args = args
+		self.source = source
 		self.output_dir = output_dir
 		self.max_width = max_width
 		self.max_height = max_height
 		self.api_key = api_key
-		self.http = httplib2.Http(".httplib2_cache")
+		self.no_text = no_text
+		self.http = None
 
-	def format_files(self):
-		for root, dirs, files in os.walk(self.source_dir):
-			for f in files:
-				if f.startswith("."):
-					continue
-				file_path = os.path.join(root, f)
+	def authenticate_http(self):
+	  flow = flow_from_clientsecrets(self.CLIENT_SECRETS_FILE, scope=self.PICASA_WEB_ALBUMS_SCOPE)
+	  storage = Storage("%s-oauth2.json" % sys.argv[0])
+	  credentials = storage.get()
+	  if credentials is None or credentials.invalid:
+	    credentials = run_flow(flow, storage, self.args)
+
+	  self.http = credentials.authorize(httplib2.Http(".httplib2_cache"))
+
+	def format_photos(self):
+		if self.source == self.GOOGLE_PLUS:
+			if not self.http:
+				self.authenticate_http()
+
+			headers, content = self.http.request(self.PICASA_WEB_ALUMBS_RECENT_PHOTOS_URL, "GET")
+			feed = json.loads(content)
+			for entry in feed["feed"]["entry"]:
 				try:
-					self.format_file(file_path=file_path)
-				except (exceptions.TypeError, exceptions.MissingDelegateError, exceptions.BlobError), e:
-					print "Error with %s: %s" % (file_path, e)
+					self.process_image(GooglePlusImage(self.http, entry))
+				except Exception, e:
+					print e
+		else:
+			for root, dirs, files in os.walk(self.source):
+				for f in files:
+					if not f.startswith("."):
+						try:
+							self.process_image(FileImage(os.path.join(root, f)))
+						except Exception, e:
+							print e
 
-	def format_file(self, file_path):
-		with Image(filename=file_path).clone() as img:
-			orientation = 1
-			taken_at = latitude = longitude = latitude_sign = longitude_sign = location = ""
+	def process_image(self, image):
+		if not image.image:
+			return
 
-			for key, value in img.metadata.items():
-				lower_key = key.lower()
-				if lower_key == "exif:orientation":
-					orientation = value
-				elif lower_key == "exif:gpslatitude":
-					latitude = value
-				elif lower_key == "exif:gpslongitude":
-					longitude = value
-				elif lower_key == "exif:gpslatituderef":
-					latitude_sign = self.DIRECTIONS_TO_SIGNS[value.lower()]
-				elif lower_key == "exif:gpslongituderef":
-					longitude_sign = self.DIRECTIONS_TO_SIGNS[value.lower()]
-				elif lower_key == "exif:datetimeoriginal":
-					taken_at = datetime.datetime.strptime(value, self.TAKEN_AT_DATE_FORMAT)
-
-			if orientation in self.ORIENTATION_TO_ROTATION:
-				img.rotate(self.ORIENTATION_TO_ROTATION[orientation])
-
-			if latitude and latitude_sign and longitude and longitude_sign and self.api_key:
-				decimal_latitude = self.dms_to_decimal(dms=latitude, sign=latitude_sign)
-				decimal_longitude = self.dms_to_decimal(dms=longitude, sign=longitude_sign)
-				location = self.reverse_geocode(latitude=decimal_latitude, longitude=decimal_longitude)
-				location = ",".join((location.split(",", 3))[0:-1])
+		with image.image.clone() as img:
+			if image.rotation_degrees:
+				img.rotate(image.rotation_degrees)
 
 			original_width, original_height = img.size
 			ratio = float(original_width) / float(original_height)
@@ -96,63 +176,62 @@ class PhotoFrameFormatter:
 			img.resize(math.trunc(new_width), math.trunc(new_height))
 			img.format = "jpeg"
 
-			text = ""
-			if taken_at:
-				text = taken_at.strftime(self.DISPLAY_DATE_FORMAT)
-			if location:
-				text += " @ " + location
-			if text:
-				with Drawing() as draw:
-					draw.font = self.FONT
-					draw.text_antialias = True
-					draw.font_size = self.FONT_SIZE
-					draw.stroke_color = self.STROKE_COLOR
-					draw.fill_color = self.FILL_COLOR
-					draw.text_alignment = "center"
-					draw.stroke_width = 0.5
-					draw.text(x=math.trunc(new_width / 2), y=self.FONT_SIZE, body=text)
-					draw(img)
+			if not self.no_text:
+				text = ""
+				if image.latitude and image.longitude:
+					location = self.reverse_geocode(latitude=image.latitude, longitude=image.longitude)
+					location = ",".join(location.split(",")[0:2])
+				else:
+					location = ""
+				if image.datetime:
+					text = image.datetime.strftime(self.DISPLAY_DATE_FORMAT)
+				if location:
+					text += " @ " + location
 
-			new_file = file_path.replace(os.sep, "").replace(".", "").replace(" ", "") + ".jpg"
-			img.save(filename=os.path.join(self.output_dir, new_file))
+				if text:
+					with Drawing() as draw:
+						draw.font = self.FONT
+						draw.text_antialias = True
+						draw.font_size = self.FONT_SIZE
+						draw.stroke_color = self.STROKE_COLOR
+						draw.fill_color = self.FILL_COLOR
+						draw.text_alignment = "center"
+						draw.stroke_width = self.STROKE_WIDTH
+						draw.text(x=math.trunc(new_width / 2), y=self.FONT_SIZE, body=text)
+						draw(img)
 
-	def dms_to_decimal(self, dms, sign):
-		factor = 1
-		total = 0
-
-		for fraction in re.split(self.SPLIT_REGEX, dms):
-			numerator, denominator = fraction.split("/")
-			quotient = float(numerator) / float(denominator) / factor
-			total += quotient
-			factor *= 60
-
-		return sign * total
+			img.save(filename=os.path.join(self.output_dir, image.filename))
 
 	def reverse_geocode(self, latitude, longitude):
 		url = self.REVERSE_GEOCDOING_URL_FORMAT.format(
 			latitude=latitude, longitude=longitude, key=self.api_key)
-		headers, content = self.http.request(url, "GET")
-		results = json.loads(content)
+
+		if not self.http:
+			self.http = httplib2.Http(".httplib2_cache")
+
 		try:
+			headers, content = self.http.request(url, "GET")
+			results = json.loads(content)
 			return results["results"][0]["formatted_address"]
-		except KeyError:
+		except Exception:
 			return ""
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description="Batch format images.")
-	parser.add_argument("--source", type=str, default=".",
-		help="The parent directory. Will be scanned recursively for images.")
-	parser.add_argument("--output", type=str, default="/tmp",
+	argparser.add_argument("--source", type=str, default=".",
+		help="The parent directory, or the string '%s' to use Google+ photos." % PhotoFrameFormatter.GOOGLE_PLUS)
+	argparser.add_argument("--destination", type=str, default="/tmp",
 		help="Directory in which formatted images will be saved.")
-	parser.add_argument("--max-width", type=int, default=800,
+	argparser.add_argument("--max-width", type=int, default=800,
 		help="Maximum width of the resized image. Aspect ratio will be preserved.")
-	parser.add_argument("--max-height", type=int, default=600,
+	argparser.add_argument("--max-height", type=int, default=600,
 		help="Maximum height of the resized image. Aspect ratio will be preserved.")
-	parser.add_argument("--api-key", type=str,
+	argparser.add_argument("--no-text", action="store_true", default=False,
+		help="Flag that disables the text overlays with date and location.")
+	argparser.add_argument("--api-key", type=str,
 		help="An optional API key. If present, reverse geocoding will be used to assign locations to images. See https://developers.google.com/maps/documentation/geocoding/#api_key")
 
-	args = parser.parse_args()
-	photo_frame_formatter = PhotoFrameFormatter(source_dir=args.source, output_dir=args.output,
-		max_width=args.max_width, max_height=args.max_height, api_key=args.api_key)
-	photo_frame_formatter.format_files()
+	args = argparser.parse_args()
+	photo_frame_formatter = PhotoFrameFormatter(args=args, source=args.source, output_dir=args.destination,
+		max_width=args.max_width, max_height=args.max_height, api_key=args.api_key, no_text=args.no_text)
+	photo_frame_formatter.format_photos()
